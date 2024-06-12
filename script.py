472bedfe97f34e5b4f17ea117272bd6862d97c40
Abdul Kadhar <abdulkadhar.sulthanbasha@ticvic.com>
2024-06-13 03:13:40 +0530

    Addition of debug trace

--- script.py
+++ script.py
@@ -5,7 +5,10 @@ image_prompt = "A retro-futuristic cityscape with neon lights and flying cars."
 
 pipe = StableDiffusionPipeline.from_pretrained(model_id)
 print(pipe)
-generated_image = pipe(image_prompt)
 
+generated_image = pipe(image_prompt)
+print("########################## Debug print for the generated image ##########################")
+print(generated_image)
+print("########################## Debug print for the generated image ##########################")
 generated_image[0]["image"].save("retro_cityscape.png")
 print("Image generated and saved!")
